8c739fa160e358633739159dc34a43b3ad347711
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-10 04:10:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum=num1+num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+sub=num2-num1
+return sub
 end
 
 def division(num1, num2)
-
+div=num2/num1
+return div
 end
 
 def multiplication(num1, num2)
-
+mult=num1*num2
+return mult
 end
 
 def modulo(num1, num2)
-
+  mod=num2%num1
+  return mod
 end
 
 def square_root(num)
-
+sq= Math.sqrt(num)
+return sq
 end
